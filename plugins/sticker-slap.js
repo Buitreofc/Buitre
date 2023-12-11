b13c9298225a3e890da64b0fbf8c3380b23bc210
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2023-12-11 21:41:36 +0100

    Actualizar sticker-slap.js

--- plugins/sticker-slap.js
+++ plugins/sticker-slap.js
@@ -12,7 +12,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     let name;
     if (who === m.chat) {
-      name = "𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝";
+      name = "𝗕𝗨𝗜𝗧𝗥𝗘 𝗕𝗢𝗧";
     } else {
       name = conn.getName(who);
     }
