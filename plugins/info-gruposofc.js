42ed30064ec1e088b2976d7b6a122706a3a8aadc
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2024-01-04 15:50:47 +0100

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix}) => {
   const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 ʙᴜɪᴛʀᴇ ʙᴏᴛ 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
 *➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-*https://chat.whatsapp.com/CmJj0wlJqAHCynm6eAHmAc`.trim();
+*1.-*https://chat.whatsapp.com/JIz9Snzgt2X4TUDmTxlnRd`.trim();
   const buttonMessage= {
     'document': {url: `http://buitre.boxmineworld.com`},
     'mimetype': `application/${document}`,
