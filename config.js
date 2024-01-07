19a3b141ff48acd80e109a493ffd015ec9ae1c8e
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2024-01-07 15:28:58 +0100

    Actualizar config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import moment from 'moment-timezone';
 
 global.owner = [
   ['50664527056', '👑 Mystic - Creador 👑', true],
-  ['506641404774','💫 Mystic - Tester Fabri115💫', true]
+  ['50661404774','💫 Mystic - Tester Fabri115💫', true]
 ];
 
 global.suittag = ['573215231131'];
