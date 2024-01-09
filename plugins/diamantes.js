4791294917152d88a2388b3ecb91d72c2dd24c9d
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2024-01-09 19:32:32 +0100

    Actualizar diamantes.js

--- plugins/diamantes.js
+++ plugins/diamantes.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data;
+  const cristiano = (await axios.get(`https://k.top4top.io/p_2930k67vy0.jpg`)).data;
   const ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())];
-  conn.sendFile(m.chat, ronaldo, 'error.jpg', `*Siiiuuuuuu*`, m);
+  conn.sendFile(m.chat, ronaldo, 'error.jpg', `*Venta de diamantes ID aquí:  https://chat.whatsapp.com/GVweATZgOlHKmk4PmLwtZu*`, m);
 };
-// conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['cristianoronaldo', 'cr7'];
+// conn.sendButton(m.chat, "*Venta de diamantes ID aquí:  https://chat.whatsapp.com/GVweATZgOlHKmk4PmLwtZu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['diamantes', 'cr7'];
 handler.tags = ['internet'];
-handler.command = /^(cristianoronaldo|cr7)$/i;
+handler.command = /^(diamantes|diamantes)$/i;
 export default handler;
