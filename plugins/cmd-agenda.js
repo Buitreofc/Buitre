ef110d789bcb5d87da5210077cea851868cc0dda
Buitre OFC <133541990+Buitreofc@users.noreply.github.com>
2024-01-25 22:53:30 +0100

    Actualizar cmd-agenda.js

---
+++ plugins/cmd-agenda.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, {conn, command }) =>{
+
+let url = pack6 [Math.floor(Math.random() * pack6.length)]
+
+conn.sendFile(m.chat, url ,m)
+
+}
+
+handler.help = ['agendasemanal']
+
+handler.tags = ['internet']
+
+handler.command = /^(agendasemanal)$/i
+
+handler.exp = 50
+
+export default handler
+
+global.pack6 = [
+
+ 
+
+ "https://g.top4top.io/p_2946tsmnq0.jpg"
+
+  ]
